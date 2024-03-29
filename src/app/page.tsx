fb8d0426ff67e422e24e9b36c411b20770b1ac4e
CaueMaldonadoLima <caue.maldonado@ufms.br>
2024-03-29 15:36:48 -0400

    fix: add new import route to the DefaultHeader component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import DefaultHeader from "./components/DefaultHeader";
+import DefaultHeader from "@/components/DefaultHeader";
 
 export default function Home() {
   return (
